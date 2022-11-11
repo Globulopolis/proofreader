d0288303f68c9ff05f5087b6911cce8dda4a1e21
Pavel Syomin <pas.ntg@gmail.com>
2022-11-11 16:42:30 +0500

    Add class to typo form header, group form elements info a div.proofreader_form_body element, #4

--- site/layouts/form.php
+++ site/layouts/form.php
@@ -12,36 +12,37 @@
 defined('_JEXEC') or die;
 ?>
 <form id="proofreader_form" action="<?php echo $displayData['action']; ?>" class="proofreader_form" method="post">
-	<h2><?php echo JText::_('COM_PROOFREADER_HEADER'); ?></h2>
+    <h2 class="proofreader_form_header"><?php echo JText::_('COM_PROOFREADER_HEADER'); ?></h2>
 
-	<div id="proofreader_messages_container"></div>
+    <div id="proofreader_messages_container"></div>
+    <div class="proofreader_form_body">
+        <div><?php echo JText::_('COM_PROOFREADER_FIELD_TYPO_LABEL'); ?></div>
+        <div id="proofreader_typo_container" class="proofreader_typo_container"></div>
 
-	<div><?php echo JText::_('COM_PROOFREADER_FIELD_TYPO_LABEL'); ?></div>
-	<div id="proofreader_typo_container" class="proofreader_typo_container"></div>
+        <?php foreach ($displayData['form']->getFieldset('basic') as $field) : ?>
+            <div class="control-group">
+                <div class="control-label">
+                    <?php echo $field->label; ?>
+                </div>
+                <div class="controls">
+                    <?php echo $field->input; ?>
+                </div>
+            </div>
+        <?php endforeach; ?>
 
-	<?php foreach ($displayData['form']->getFieldset('basic') as $field) : ?>
-		<div class="control-group">
-			<div class="control-label">
-				<?php echo $field->label; ?>
-			</div>
-			<div class="controls">
-				<?php echo $field->input; ?>
-			</div>
-		</div>
-	<?php endforeach; ?>
-
-	<div class="control-group">
-		<div class="controls">
-			<button type="submit" id="proofreader_submit" class="btn btn-primary">
-				<i class="icon-ok"></i>
-				<?php echo JText::_('JSUBMIT'); ?>
-			</button>
-		</div>
-	</div>
-	<div>
-		<?php foreach ($displayData['form']->getFieldset('hidden') as $field) : ?>
-			<?php echo $field->input; ?>
-		<?php endforeach; ?>
-		<?php echo JHtml::_('form.token'); ?>
-	</div>
+        <div class="control-group">
+            <div class="controls">
+                <button type="submit" id="proofreader_submit" class="btn btn-primary">
+                    <i class="icon-ok"></i>
+                    <?php echo JText::_('JSUBMIT'); ?>
+                </button>
+            </div>
+        </div>
+        <div>
+            <?php foreach ($displayData['form']->getFieldset('hidden') as $field) : ?>
+                <?php echo $field->input; ?>
+            <?php endforeach; ?>
+            <?php echo JHtml::_('form.token'); ?>
+        </div>
+    </div>
 </form>
