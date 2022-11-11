b5c67c07f8dd3797cdcd8a5355d568d1da0aa235
Pavel Syomin <pas.ntg@gmail.com>
2022-11-11 16:25:15 +0500

    Fix a SQL warning, #2

--- administrator/sql/updates/mysql/2.0.1.sql
+++ administrator/sql/updates/mysql/2.0.1.sql
@@ -1,7 +1,7 @@
 DROP INDEX `idx_language` ON `#__proofreader_typos`;
 
-ALTER TABLE `#__proofreader_typos` CHANGE COLUMN `language` `page_language` VARCHAR(255) NOT NULL DEFAULT '' AFTER `page_title`;
-ALTER TABLE `#__proofreader_typos` CHANGE COLUMN `ip` `created_by_ip` VARCHAR(39) NOT NULL DEFAULT '' AFTER `created_by`;
-ALTER TABLE `#__proofreader_typos` CHANGE COLUMN `comment` `typo_comment` TEXT NOT NULL DEFAULT '';
+ALTER TABLE `#__proofreader_typos` RENAME COLUMN `language` TO `page_language`;
+ALTER TABLE `#__proofreader_typos` RENAME COLUMN `ip` TO `created_by_ip`;
+ALTER TABLE `#__proofreader_typos` RENAME COLUMN `comment` TO `typo_comment`;
 
 CREATE INDEX `idx_page_language` ON `#__proofreader_typos`(`page_language`);
