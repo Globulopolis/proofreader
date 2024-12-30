cfaa8668077716dd46d7abe1605c2f371ae21793
Globulopolis <web@libra.ms>
2024-12-30 19:19:24 +0300

    Adopt content plugin to J5 MVC

---
+++ plugins/content/services/provider.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Proofreader
+ *
+ * @package     Proofreader
+ * @author      Sergey M. Litvinov (smart@joomlatune.com)
+ * @copyright   Copyright (C) 2013-2015 by Sergey M. Litvinov. All rights reserved.
+ * @copyright   Copyright (C) 2005-2007 by Alexandr Balashov. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Extension\PluginInterface;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Plugin\PluginHelper;
+use Joomla\DI\Container;
+use Joomla\DI\ServiceProviderInterface;
+use Joomla\Event\DispatcherInterface;
+use Joomla\Plugin\Content\Proofreader\Extension\Proofreader;
+
+return new class implements ServiceProviderInterface {
+	/**
+	 * Registers the service provider with a DI container.
+	 *
+	 * @param   Container  $container  The DI container.
+	 *
+	 * @return  void
+	 *
+	 * @since   4.3.0
+	 */
+	public function register(Container $container)
+	{
+		$container->set(
+			PluginInterface::class,
+			function (Container $container)
+			{
+				$plugin = new Proofreader(
+					$container->get(DispatcherInterface::class),
+					(array) PluginHelper::getPlugin('content', 'proofreader')
+				);
+				$plugin->setApplication(Factory::getApplication());
+
+				return $plugin;
+			}
+		);
+	}
+};
